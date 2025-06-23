c5a67b85e40dceb73b4746fcbee6397b41f42aaa
thetygerpotatto <letras.cj@gmail.com>
2025-06-23 12:46:18 -0500

    ahora en el puerto 80

--- run.py
+++ run.py
@@ -6,4 +6,4 @@ socket = createApp.socketio
 app = createApp.create_app()
 
 if __name__ == '__main__':
-    socket.run(app,host="0.0.0.0", debug=True)
+    socket.run(app,host="0.0.0.0", port=80, debug=True)
